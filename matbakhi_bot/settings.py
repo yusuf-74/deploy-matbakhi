4675402604c302b737c4e621f8c0ac8bfb8b275f
yusuf-74 <yachour31@gmail.com>
2023-03-21 19:25:29 +0200

    fixing some issues

--- matbakhi_bot/settings.py
+++ matbakhi_bot/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 from decouple import config
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS =  [BASE_DIR / '/assets']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
